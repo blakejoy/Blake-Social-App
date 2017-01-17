5cef027f818f1aef95a71863ffb377e65a48a720
Blake Joynes <blakejoynes@Blakes-MacBook-Pro.local>
2017-01-16 23:24:49 -0500

    Completed setting up Firebase

---
+++ Blake Social/SignInVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Blake Social
+//
+//  Created by Blake Joynes on 1/16/17.
+//  Copyright © 2017 BJ. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+ 
+
+}
+
---
+++ Pods/Target Support Files/Pods-Blake Social/Pods-Blake Social-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Blake_SocialVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Blake_SocialVersionString[];
+
